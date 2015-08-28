f22603b581acfe97d501869a8b3395f41e9aad8c
5n4k3 <5n4k3@snakepitsoftware.com>
2015-08-28 14:50:46 -0400

    Added deltawalker diff and merge tools to linux config
    Signed-off-by: 5n4k3 <5n4k3@snakepitsoftware.com>

--- create-links.sh
+++ create-links.sh
@@ -47,7 +47,7 @@ KERNEL_NAME=$(uname -s | tr "[:upper:]" "[:lower:]")
 #
 
 # Non shell specific
-DOTS="digrc nslookuprc nofinger gitignore vimrc gitconfig_common gitconfig_mac"
+DOTS="digrc nslookuprc nofinger gitignore vimrc gitconfig_common gitconfig_mac gitconfig_linux"
 # BASH shell
 DOTS="${DOTS} bash_profile bashrc bash_logout bash_alias"
 # SH shell (including ash, dash, ...)
